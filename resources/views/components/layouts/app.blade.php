e33daaf40dd51742155323dad62e0d4d6d9a363c
andfelipeme <andfelipeme@gmail.com>
2026-02-25 17:13:52 -0500

    Se agrega el favicon del project

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
     <title>{{ $title }}</title>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
